fcc45f0a10e45925d3b1a9ea0e9e755d1ca5f6a7
lina-jamal <linahjamal89@gmail.com>
2021-07-12 10:48:51 +0300

    add local storage quations

---
+++ script.js
@@ -0,0 +1,7 @@
+// write function that take the key an , brand and price for the car ,These values are then passed to the car object and stored in local storage
+
+// wrie afunction that take the car key from user and then get the item value  from local storage whish have the key
+
+//write function that delete a specific value from the local storage
+
+//writ function  to remove all values in the local storage.
